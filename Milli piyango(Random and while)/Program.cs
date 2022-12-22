b20a1a1cd20fcde9b80b8bf95b0e54da614b3b95
MUHAMMED <m.yldrm3444@gmail.com>
2022-12-22 18:45:16 +0300

    Random and while

---
+++ Milli piyango(Random and while)/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Milli_piyango_Random_and_while_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] kazananBiletler = { 123, 562, 297, 476, 821, 401, 500, 632 };
+            int benimBiletim = 0;
+            int denemeSayisi = 0;
+            bool kazandimMi = false;
+
+            Random random = new Random();
+
+            while(!kazandimMi)
+            {
+                benimBiletim = random.Next(1000);
+                denemeSayisi++;
+
+                foreach (var bilet in kazananBiletler)
+                {
+                    if(bilet==benimBiletim)
+                    {
+                        kazandimMi = true;
+                        Console.WriteLine("Tebrikler,kazandınız");
+                        Console.WriteLine("Deneme Sayısı:" + denemeSayisi.ToString());
+                    }
+
+                }
+            }
+        }
+    }
+}
